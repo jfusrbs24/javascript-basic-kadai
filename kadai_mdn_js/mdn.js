771bf653d514d0093960464b7e8672dcbc040d58
jfusrbs24 <lojigydr@outlook.jp>
2025-10-16 00:09:16 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const getDate = new Date();
+
+const getYear = getDate.getFullYear();
+
+const getMonth = getDate.getMonth();
+
+const getDay = getDate.getDate();
+
+console.log(getYear + '年' + getMonth + '月' + getDay + '日'); 
